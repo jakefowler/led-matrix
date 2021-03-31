50c40a25d0271c221c15771cc061f9e970ac3c20
jakefowler <46506569+jakefowler@users.noreply.github.com>
2021-03-31 05:58:11 -0600

    added choosing a color for the scrolling text

---
+++ LedMatrix/LedMatrix/wwwroot/js/colorPicker.js
@@ -0,0 +1,26 @@
+﻿
+let cellColor = "rgb(255, 0, 0)";
+const colorCellSize = 2;
+const scaleDown = 8;
+let colorPickerCanvas = document.getElementById("colorPicker");
+let colorPickerContext = colorPickerCanvas.getContext("2d");
+
+function fillColorPicker(e) {
+    let blue = $('#blueSlider').val();
+    for (let r = 0; r < 255; r+=scaleDown) {
+        for (let g = 0; g < 255; g+=scaleDown) {
+            colorPickerContext.fillStyle = `rgb(${r},${g},${blue}`;
+            colorPickerContext.fillRect(r * colorCellSize, g * colorCellSize, colorCellSize * scaleDown, colorCellSize * scaleDown);
+        }
+    }
+}
+
+$(document).ready(function () {
+    fillColorPicker();
+    $('#blueSlider').on('input', fillColorPicker);
+    $('#colorPicker').click(function (event) {
+        let blue = $('#blueSlider').val();
+        cellColor = `rgb(${Math.floor(event.offsetX / colorCellSize)}, ${Math.floor(event.offsetY / colorCellSize)}, ${blue})`
+        $('#selectedColor').css('background-color', cellColor);
+    });
+});
